ad27628cd0354035a3d12e6021c0a7b72467c5d3
Rémi Côme <remi.come@gmx.com>
2025-10-06 21:33:13 +0200

    feat: importe des labels

--- src/expenses/_ihatemoney.py
+++ src/expenses/_ihatemoney.py
@@ -1,6 +1,5 @@
 """Module gérant l'interface avec I Hate Money."""
 
-import datetime
 import os
 
 import pandas as pd
@@ -9,15 +8,24 @@ from .expense import Expense
 
 
 def read_ihatemoney_csv(path: os.PathLike) -> list[Expense]:
-    """Lit un fichier tel qu'exporté par I Hate Money."""
+    """
+    Lit un fichier tel qu'exporté par I Hate Money.
+
+    On ajoute à la spécification initiale la possibilité d'inclure une colonne "label",
+    qui permet d'associer une étiquette à certaines dépenses.
+    """
     dtype = {
         "what": pd.StringDtype(),
         "amount": float,
         "date": str,
         "payer_name": str,
         "owers": str,
+        "label": str,
     }
     df = pd.read_csv(path, sep=";", dtype=dtype)
+
+    if "label" not in df:
+        df["label"] = None
 
     return [
         Expense(
@@ -26,6 +34,7 @@ def read_ihatemoney_csv(path: os.PathLike) -> list[Expense]:
             who_for=_who_for(row.owers),
             when=row.date,
             description="" if pd.isnull(row.what) else row.what,
+            label=None if pd.isnull(row.label) else row.label,
         )
         for _, row in df.iterrows()
     ]
--- tests/test_ihatemoney.py
+++ tests/test_ihatemoney.py
@@ -14,12 +14,21 @@ def test_import(original_expenses: Expenses, ihatemoney_file: pathlib.Path) -> N
     """Teste l'import."""
 
     imported_expenses = Expenses.from_ihatemoney(ihatemoney_file)
-    assert all(
-        imported_expense == original_expense
-        for imported_expense, original_expense in zip(
-            imported_expenses, original_expenses
-        )
-    )
+    assert imported_expenses == original_expenses
+
+
+def test_import_with_labels(
+    original_expenses_with_labels: Expenses,
+    ihatemoney_file_with_labels: pathlib.Path,
+) -> None:
+    """
+    Teste l'import de labels.
+
+    Ceux-ci ne font pas partie du format I Hate Money de base. Ils sont pensés pour être
+    ajoutés facilement à la main en post-traitement.
+    """
+    imported_expenses = Expenses.from_ihatemoney(ihatemoney_file_with_labels)
+    assert imported_expenses == original_expenses_with_labels
 
 
 @pytest.fixture
@@ -32,6 +41,12 @@ def original_expenses(expenses: Expenses) -> Expenses:
     return Expenses(
         [expense.model_copy(update={"label": None}) for expense in expenses]
     )
+
+
+@pytest.fixture
+def original_expenses_with_labels(expenses: Expenses) -> Expenses:
+    """La liste des dépenses enregistrées à travers I Hate Money, avec étiquettes."""
+    return expenses
 
 
 @pytest.fixture
@@ -49,6 +64,32 @@ def ihatemoney_file(expenses: Expenses) -> typing.Iterator[pathlib.Path]:
                 "date": expense.when.strftime("%Y-%m-%d"),
                 "payer_name": expense.who_paid,
                 "owers": ",".join(expense.who_for),
+            }
+            for expense in expenses
+        ]
+    )
+    with tempfile.TemporaryDirectory() as directory:
+        destination = pathlib.Path(directory) / "ihatemoney.csv"
+        expenses.to_csv(destination, sep=";", index=False)
+        yield destination
+
+
+@pytest.fixture
+def ihatemoney_file_with_labels(expenses: Expenses) -> typing.Iterator[pathlib.Path]:
+    """
+    Un fichier de dépense au format exporté par I Hate Money.
+
+    On ajoute au fichier initial des étiquettes dans la colonne "label".
+    """
+    expenses = pd.DataFrame(
+        [
+            {
+                "what": expense.description,
+                "amount": expense.amount,
+                "date": expense.when.strftime("%Y-%m-%d"),
+                "payer_name": expense.who_paid,
+                "owers": ",".join(expense.who_for),
+                "label": expense.label,
             }
             for expense in expenses
         ]
